459c3fd0c1690fdd39a257fc35f74f40a9eee4ca
Developer <developer@IM-L12-367.local>
2017-07-25 14:53:05 -0500

    Agregar tabla con datos del JSON

--- index2.js
+++ index2.js
@@ -5,6 +5,12 @@ const hostname = '127.0.0.1';
 const PORT = 3000;
 app.listen(PORT);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
   res.send('Main page');
 });
--- riotapi2.js
+++ riotapi2.js
@@ -1,7 +1,7 @@
 const request = require("request");
 let summonerid = 59627;
 let server = "la1";
-const API_KEY = "RGAPI-42acd204-1906-4b0f-aba2-aa69dd66099f";
+const API_KEY = "RGAPI-f35372e9-c045-48ca-bcd1-6bd653e9deac";
 let requestedToday = 0;
 
 function buildUrl(summonerId, server, endpoint) {
