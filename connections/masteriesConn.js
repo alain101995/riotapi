c4dbaa220015a7cb86eff236f03eee13f43f243c
alain101995 <alain101995@gmail.com>
2017-09-29 15:10:53 -0500

    Find by fields already working

--- connections/masteriesConn.js
+++ connections/masteriesConn.js
@@ -5,32 +5,14 @@ mongoose.connect('mongodb://localhost/test', { useMongoClient: true, promiseLibr
 const masteriesSchema = new Schema({
     expireAt: {},
     summonerId: Number,
-    pages: [{
-        id: Number,
-        name: String,
-        current: Boolean,
-        masteries: [{
-            id: Number,
-            rank: Number
-        }],
-    }]
+    pages: Array
 });
 
 const masteriesModel = mongoose.model('masteries', masteriesSchema);
 dbMasteries = function (data) {
-    for (let masteriesOf of data.pages) {
-        let masteriesData = new masteriesModel({
-            expireAt: new Date().getTime() + 1 * 24 * 60 * 60000, // new Date('September 25, 2017 14:00:00')   new Date().getTime() + 1 * 24 * 60 * 60000
-            summonerId: data.summonerId,
-            pages: [{
-                id: masteriesOf.id,
-                name: masteriesOf.name,
-                current: masteriesOf.current,
-                masteries: [{
-                    id: masteriesOf.masteries.id,
-                    rank: masteriesOf.masters.rank
-                }],
-            }]
-        });
-    }
+    let masteriesData = new masteriesModel({
+        expireAt: new Date().getTime() + 1 * 24 * 60 * 60000, // new Date('September 25, 2017 14:00:00')   new Date().getTime() + 1 * 24 * 60 * 60000
+        summonerId: data.summonerId,
+        pages: data.pages
+    });
 }
--- connections/runesConn.js
+++ connections/runesConn.js
@@ -13,6 +13,9 @@ const oneDay = seconds * minutes * hour * days;
 const runesSchema = new Schema({
   expireAt: Date,
   summonerId: Number,
+  pages: Array
+});
+/*
   pages: [{
     id: Number,
     name: String,
@@ -21,44 +24,43 @@ const runesSchema = new Schema({
       runeSlotId: Number,
       runeId: Number
     }]
-  }]
-});
-
+    }]
+    */
 const Runes = mongoose.model('runes', runesSchema);
 
 function create(runes) {
-  const expireAt = new Date(new Date().getTime() + oneDay), // new Date('September 25, 2017 14:00:00')   new Date().getTime() + 1 * 24 * 60 * 60000
+  const expireAt = new Date(new Date().getTime() + oneDay), // new Date('September 25, 2017 14:00:00') new Date(new Date().getTime() + oneDay)  new Date().getTime() + 1 * 24 * 60 * 60000
     runesData = new Runes({ // <== was const runesData
       expireAt,
       summonerId: runes.summonerId,
       pages: runes.pages
     });
-  console.log(runesData)
+  console.log('CREATED', runesData)
   return runesData.save();
 }
 
 function findInRunesDb(summonerId) {
   return new Promise(function (resolve, reject) {
-    let sumInDb = Runes.findOne({ 'summonerId': summonerId }, function (err, summonerId) {
+    Runes.find({}, 'expireAt summonerId pages', function (err, runesData) {
+      console.log('Runes Data', runesData);
 
-      if (!summonerId)  {
-        console.log('Theres no data in db', summonerId) // Has data on database
-        return (err); // Check if sumID exist in db <===
-      }
-      if (summonerId.expireAt < new Date(new Date()).getTime()) {
+      // Runes.findOne('summonerId', function (err, runesData) {}) // ...
+      if (runesData.expireAt < new Date(new Date().getTime())) {
         // Added + 1 * 24 * 60 * 60000 so the current date is gonna be bigger than expiration date on current data on database (// 9/27/2017, 2:42:03 PM). But it needs to be skipped
-        console.log('Data life time', summonerId.expireAt, 'My Time + 1 Day', new Date(new Date().getTime() + oneDay));
-        resolve(summonerId); // summonerId.summonerId or boolean
+        console.log('Data expired', runesData.expireAt, 'My Time', new Date(new Date().getTime()));
+        resolve(true); // summonerId.summonerId or boolean
       } else {
-        console.log('Not expired yet')
+        console.log('Not expired yet', runesData.expireAt, 'My Time', new Date(new Date().getTime()));
         resolve(false);
       }
       reject(err);
     })
+
+    // summonerId.expireAt < new Date(new Date()).getTime()
   });
 }
 
-function remove() {
+function remove(summonerId) {
   runesData.remove({}, function (error) { // Remove not working because slots: runeSlotId and runeId is undefined
     if (error) return ('Error: ', error);
     console.log('Data removed')
